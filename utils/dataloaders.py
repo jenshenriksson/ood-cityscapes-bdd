6bca709389714c498a4f7ee6312ab392c1291e3f
jenshenriksson <jens.henriksson.91@gmail.com>
2022-08-19 14:07:03 +0200

    Remade dataloader to allow for preloading of images

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,11 +1,15 @@
 import cv2 
 import numpy as np
 from torch.utils.data import Dataset as Dataset
-from parameters import * 
+# from parameters import * 
 import sys
 sys.path.append('..')
 import utils as CU
+import time
 
+from tqdm.notebook import tqdm
+import albumentations as albu
+from albumentations.augmentations.geometric.resize import Resize
 
 class CityScapes(Dataset):
     """
@@ -80,20 +84,19 @@ class CityScapes(Dataset):
             classes = self.CLASSES
         
         self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
-        
         self.augmentation = augmentation
         self.preprocessing = preprocessing
     
     def __getitem__(self, i):
-        
         # read data
         image = cv2.imread(self.images_fps[i])
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         mask = cv2.imread(self.masks_fps[i], 0)
-        
+
         # extract certain classes from mask (e.g. cars)
         masks = [(mask == v) for v in self.class_values]
         mask = np.stack(masks, axis=-1).astype('float')
+
         
         # apply augmentations
         if self.augmentation:
@@ -104,7 +107,7 @@ class CityScapes(Dataset):
         if self.preprocessing:
             sample = self.preprocessing(image=image, mask=mask)
             image, mask = sample['image'], sample['mask']
-            
+
         return image, mask
         
     def __len__(self):
@@ -158,9 +161,11 @@ class BDD100K(Dataset):
             classes=None, 
             augmentation=None, 
             preprocessing=None,
+            preload = False, 
     ):
         self.images_fps = images
         self.masks_fps = labels
+        self.preload = preload 
         
         # convert str names to class values on masks
         if not classes: 
@@ -170,28 +175,51 @@ class BDD100K(Dataset):
         
         self.augmentation = augmentation
         self.preprocessing = preprocessing
+        
+                
+        # Store images before dataloader. 
+        if self.preload: 
+            self.preloadedImages = []
+            self.preloadedMasks = [] 
+            
+            albuResize = albu.Compose([
+                Resize(height=256, width=512)
+            ])
+            for i in tqdm(range(len(self.images_fps))):
+                image = cv2.imread(self.images_fps[i])
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                mask = cv2.imread(self.masks_fps[i], 0)
+                
+                # Resize
+                image = albuResize(image=image)['image']
+                mask = albuResize(image=mask)['image']
+                
+                self.preloadedImages.append(image)
+                self.preloadedMasks.append(mask) 
     
     def __getitem__(self, i):
-        
-        # read data
-        image = cv2.imread(self.images_fps[i])
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        mask = cv2.imread(self.masks_fps[i], 0)
-        
-        # extract certain classes from mask (e.g. cars)
+        if not self.preload: 
+            # read data
+            image = cv2.imread(self.images_fps[i])
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            mask = cv2.imread(self.masks_fps[i], 0)
+        else: 
+            image = self.preloadedImages[i]
+            mask = self.preloadedMasks[i]
+
+        # extract certain classes from mask + convert to one-hot. 
         masks = [(mask == v) for v in self.class_values]
         mask = np.stack(masks, axis=-1).astype('float')
-        
         # apply augmentations
         if self.augmentation:
             sample = self.augmentation(image=image, mask=mask)
             image, mask = sample['image'], sample['mask']
-        
+
         # apply preprocessing
         if self.preprocessing:
             sample = self.preprocessing(image=image, mask=mask)
             image, mask = sample['image'], sample['mask']
-            
+        
         return image, mask
         
     def __len__(self):
@@ -278,9 +306,11 @@ class CityScapes_class_merge(Dataset):
             classes=None, 
             augmentation=None, 
             preprocessing=None,
+            preload=False, 
     ):
         self.images_fps = images
         self.masks_fps = labels
+        self.preload = preload 
         
         # convert str names to class values on masks
         if not classes: 
@@ -308,14 +338,38 @@ class CityScapes_class_merge(Dataset):
         
         self.augmentation = augmentation
         self.preprocessing = preprocessing
+                
+        # Store images before dataloader. 
+        if self.preload: 
+            self.preloadedImages = []
+            self.preloadedMasks = [] 
+            
+            albuResize = albu.Compose([
+                Resize(height=256, width=512)
+            ])
+            for i in tqdm(range(len(self.images_fps))):
+                image = cv2.imread(self.images_fps[i])
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                mask = cv2.imread(self.masks_fps[i], 0)
+                
+                # Resize
+                image = albuResize(image=image)['image']
+                mask = albuResize(image=mask)['image']
+                
+                self.preloadedImages.append(image)
+                self.preloadedMasks.append(mask) 
+        
     
     def __getitem__(self, i):
         
         # read data
-        image = cv2.imread(self.images_fps[i])
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        mask = cv2.imread(self.masks_fps[i], 0)
-        
+        if not self.preload: 
+            image = cv2.imread(self.images_fps[i])
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            mask = cv2.imread(self.masks_fps[i], 0)
+        else: 
+            image = self.preloadedImages[i]
+            mask = self.preloadedMasks[i]        
         
         # extract certain classes from mask (e.g. cars)
         masks = list()
